c9e624d880b1fbdf6051fac139d4a3d29a306bc5
Jufrench <ju.french@gmail.com>
2024-03-23 22:41:10 -0500

    Fixed mobiles resizing issue. 100vh in desktop browser isn't the same as 100vh in mobile browser

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,27 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  // Mozilla-based browsers will ignore this.
+  const webkitStyle = {height: "-webkit-fill-available"};
+  // WebKit-based browsers will ignore this.
+  const mozillaStyle = {height: "-webkit-fill-available"};
+
+  
+
   return (
     <Html lang="en">
       <Head />
-      <body style={{height: "100vh"}}>
+      <body
+        // style={{
+        //   // height: "100vh",
+        //   // position: "relative",
+
+        //   height: "100dvh",
+        //   maxHeight: "-webkit-fill-available",
+        //   // height: "fill-available"
+        // }}
+        style={{...webkitStyle, ...mozillaStyle}}
+        >
         <Main />
         <NextScript />
       </body>
